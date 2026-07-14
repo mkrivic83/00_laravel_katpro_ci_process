463e240d36c3dd13f485338e130dd5df189651a5
mkrivic83 <krivic.milan@gmail.com>
2026-07-14 18:59:38 +0200

    dev okolina

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(app()->environment('production')){
+        if(app()->environment('production') || app()->environment('production')){
             URL::forceScheme('https');
         }
         Gate::define('admin-access',function ($user){
