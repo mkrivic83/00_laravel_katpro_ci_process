fc0116d9261b506b7e3fbdc2c2aa6ad9f4ff9193
mkrivic83 <krivic.milan@gmail.com>
2026-07-14 20:36:16 +0200

    https za prod - popravak

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(app()->environment('educ')){
+        if(app()->environment('prod')){
             URL::forceScheme('https');
         }
         Gate::define('admin-access',function ($user){
